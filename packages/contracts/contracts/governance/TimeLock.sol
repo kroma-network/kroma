9e35a8d111c7783964b1083d3d7ce72daad3cdac
Ayaan <kangrangx@gmail.com>
2023-07-26 18:31:25 +0900

    feat(contracts): add contracts about `Multi sig based proxy upgrade`.

---
+++ packages/contracts/contracts/governance/TimeLock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import { Semver } from "../universal/Semver.sol";
+
+/**
+ * @custom:proxied
+ * @title TimeLock
+ * @notice The TimeLock is a timelock controller based on OpenZeppelin TimelockController.
+ */
+contract TimeLock is Initializable, TimelockControllerUpgradeable, Semver {
+    /**
+     * @custom:semver 0.1.0
+     */
+    constructor() Semver(0, 1, 0) {}
+
+    /**
+     * @notice Initializer.
+     *
+     * @param _minDelay  Initial minimum delay for operations.
+     * @param _proposers Accounts to be granted proposer and canceller roles.
+     * @param _executors Accounts to be granted executor role.
+     * @param _admin     Optional account to be granted admin role; disable with zero address.
+     */
+    function initialize(
+        uint256 _minDelay,
+        address[] memory _proposers,
+        address[] memory _executors,
+        address _admin
+    ) public initializer {
+        __TimelockController_init(_minDelay, _proposers, _executors, _admin);
+    }
+}
