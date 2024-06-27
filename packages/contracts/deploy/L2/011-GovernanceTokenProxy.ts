2277883c523adf04d93859083f827a4cd182117d
seolaoh <osa8361@gmail.com>
2024-06-27 20:01:42 +0900

    feat(contracts): upgrade `GovernanceTokenProxy` by deployer

--- packages/contracts/deploy/L2/011-GovernanceTokenProxy.ts
+++ packages/contracts/deploy/L2/011-GovernanceTokenProxy.ts
@@ -3,13 +3,16 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 import { deployDeterministicProxy } from '../../src/deploy-utils'
 
 const deployFn: DeployFunction = async (hre) => {
+  // Deploy proxy contract with deployer as admin
+  const { deployer } = await hre.getNamedAccounts()
+
   const l1 = hre.network.companionNetworks['l1']
   const deployConfig = hre.getDeployConfig(l1)
 
   await deployDeterministicProxy(
     hre,
     'GovernanceTokenProxy',
-    deployConfig.mintManagerOwner,
+    deployer,
     deployConfig.governanceTokenProxySalt
   )
 }
--- packages/contracts/deploy/L2/013-GovernanceToken.ts
+++ packages/contracts/deploy/L2/013-GovernanceToken.ts
@@ -2,18 +2,25 @@ import '@kroma/hardhat-deploy-config'
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 import { predeploys } from '../../src'
-import { assertContractVariable, deploy } from '../../src/deploy-utils'
+import {
+  assertContractVariable,
+  deployAndUpgradeByDeployer,
+  getDeploymentAddress,
+} from '../../src/deploy-utils'
 
 const deployFn: DeployFunction = async (hre) => {
-  const Artifact__L1GovernanceTokenProxy = await hre.companionNetworks[
-    'l1'
-  ].deployments.get('L1GovernanceTokenProxy')
+  const l1 = hre.network.companionNetworks['l1']
+  const deployConfig = hre.getDeployConfig(l1)
 
-  await deploy(hre, 'GovernanceToken', {
-    args: [
-      predeploys.L2StandardBridge,
-      Artifact__L1GovernanceTokenProxy.address,
-    ],
+  const governanceTokenProxyAddress = await getDeploymentAddress(
+    hre,
+    'GovernanceTokenProxy'
+  )
+  const mintManagerAddress = await getDeploymentAddress(hre, 'MintManager')
+
+  await deployAndUpgradeByDeployer(hre, 'GovernanceToken', {
+    args: [predeploys.L2StandardBridge, governanceTokenProxyAddress],
+    initArgs: [mintManagerAddress],
     postDeployAction: async (contract) => {
       await assertContractVariable(
         contract,
@@ -23,10 +30,31 @@ const deployFn: DeployFunction = async (hre) => {
       await assertContractVariable(
         contract,
         'REMOTE_TOKEN',
-        Artifact__L1GovernanceTokenProxy.address
+        governanceTokenProxyAddress
       )
     },
   })
+
+  // Ensure variables are set correctly after initialization
+  const upgradedProxy = await hre.ethers.getContractAt(
+    'GovernanceToken',
+    governanceTokenProxyAddress
+  )
+  assertContractVariable(upgradedProxy, 'owner', mintManagerAddress)
+
+  // Change admin of GovernanceTokenProxy to mintManagerOwner
+  const { deployer } = await hre.getNamedAccounts()
+  const signer = hre.ethers.provider.getSigner(deployer)
+  const governanceTokenProxy = await hre.ethers.getContractAt(
+    'Proxy',
+    governanceTokenProxyAddress
+  )
+  const contractWithSigner = governanceTokenProxy.connect(signer)
+  const tx = await contractWithSigner.changeAdmin(deployConfig.mintManagerOwner)
+  await tx.wait()
+  console.log(
+    `changed admin of "GovernanceTokenProxy" to ${deployConfig.mintManagerOwner}`
+  )
 }
 
 deployFn.tags = ['GovernanceToken', 'l2', 'tge']
