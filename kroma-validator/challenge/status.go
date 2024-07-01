d7d1c115f289431fe8892c91312976ba4860541f
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-07-01 17:42:36 +0300

    Update kroma-validator/challenge/status.go
    Co-authored-by: Hansol Lee <38912532+0xHansLee@users.noreply.github.com>

--- kroma-validator/challenge/status.go
+++ kroma-validator/challenge/status.go
@@ -2,7 +2,7 @@ package challenge
 
 const (
 	// StatusNone is regarded as a challenge that is not in progress.
-	// The other status is regarded as a challenge in progress.
+	// The others are regarded as a challenge in progress.
 	StatusNone uint8 = iota
 	StatusChallengerTurn
 	StatusAsserterTurn
