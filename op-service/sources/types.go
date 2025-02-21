4e9b99ec8ed3f92ec74fa2e08a7e2d00a1083e3b
Ayaan <kangrangx@gmail.com>
2025-02-21 18:12:42 +0900

    feat: apply requestsHash and SetCodeTxType to support l1 Pectra upgrade (#435)
    * op-service: add `RequestsHash` to `RPCHeader` (#14022)
    
    * add RequestsHash to op-service RPCHeader type
    
    * sources.RPCHeader.RequestsHash needs "requestsHash" json struct tag
    
    not "requestsHash" as in geth
    
    * remove comment
    
    This was fixed upstream ethereum/go-ethereum#30926
    
    * add comment
    
    * Update op-service/sources/types.go
    
    Co-authored-by: protolambda <proto@protolambda.com>
    
    ---------
    
    Co-authored-by: protolambda <proto@protolambda.com>
    
    * feat: ignore SetCode batcher txs
    - source: https://github.com/ethereum-optimism/optimism/pull/13958/commits
    
    * chore: update geth dependency
    
    ---------
    
    Co-authored-by: George Knee <georgeknee@googlemail.com>
    Co-authored-by: protolambda <proto@protolambda.com>

--- op-service/sources/types.go
+++ op-service/sources/types.go
@@ -157,6 +157,9 @@ type RPCHeader struct {
 	// ParentBeaconRoot was added by EIP-4788 and is ignored in legacy headers.
 	ParentBeaconRoot *common.Hash `json:"parentBeaconBlockRoot,omitempty"`
 
+	// RequestsHash was added by EIP-7685 and is ignored in legacy headers.
+	RequestsHash *common.Hash `json:"requestsHash,omitempty" rlp:"optional"`
+
 	// untrusted info included by RPC, may have to be checked
 	Hash common.Hash `json:"hash"`
 }
@@ -212,6 +215,8 @@ func (hdr *RPCHeader) createGethHeader() *types.Header {
 		BlobGasUsed:      (*uint64)(hdr.BlobGasUsed),
 		ExcessBlobGas:    (*uint64)(hdr.ExcessBlobGas),
 		ParentBeaconRoot: hdr.ParentBeaconRoot,
+		// Prague
+		RequestsHash: hdr.RequestsHash,
 	}
 }
 
